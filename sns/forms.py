846c8517a8f1ee3fde72ea5425ce48d54341c0e3
libra-murayama <neugate.murayama@gmail.com>
2024-03-04 13:35:27 +0900

    form,view,url

--- sns/forms.py
+++ sns/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from.models import Message,Good
 from django.contrib.auth.models import User
 
+
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
